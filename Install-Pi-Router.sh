d5c7c7d6050bb228807a9f6155e4030936993381
jwepdx <37279570+jwepdx@users.noreply.github.com>
2018-08-01 23:16:27 -0700

    Update the Install Script

--- Install-Pi-Router.sh
+++ Install-Pi-Router.sh
@@ -8,8 +8,21 @@ if [ "$(id -u)" != "0" ]; then
   exec sudo "$0" "$@"
 fi
 
-#Variables
+#Update Packages
+apt-get update
+apt-get upgrade
 
+#Variables
+	#Dialog Sizing
+	screen_size=$(stty size 2>/dev/null || echo 24 80)
+	rows=$(echo "${screen_size}" | awk '{print $1}')
+	columns=$(echo "${screen_size}" | awk '{print $2}')
+	# Divide by two so the dialogs take up half of the screen, which looks nice.
+	r=$(( rows / 2 ))
+	c=$(( columns / 2 ))
+	# Unless the screen is tiny
+	r=$(( r < 20 ? 20 : r ))
+	c=$(( c < 70 ? 70 : c ))
 
 #Download Files
 GitFiles () {
@@ -19,7 +32,7 @@ GitFiles () {
 #Install Required Packages
 InstallPackages () {
 #With 'apt-get'
-apt-get install unbound hostapd bridge-utils iptables openssl
+apt-get install unbound hostapd iproute2 iptables openssl
 }
 
 #Back-up Original Files
@@ -29,6 +42,36 @@ Back-up () {
 
 #Install recursive DNS Server
 Unbound () {
+#Define File Movements
+IPv4 () {
+	mv /etc/pi-router/configs/unbound.conf-ipv4 /etc/unbound/unbound.conf.d/pi-router.conf
+}
+IPv6 () {
+	mv /etc/pi-router/configs/unbound.conf-ipv6 /etc/unbound/unbound.conf.d/pi-router.conf
+}
+Both () {
+	mv /etc/pi-router/configs/unbound.conf-ipv4 /etc/unbound/unbound.conf.d/pi-router.conf
+}
+
+#Get list of root servers and move them into place
+wget -O root.hints https://www.internic.net/domain/named.root 
+mv root.hints /var/lib/unbound/
+
+#Choose Which Config To Use
+whiptail --backtitle "IP" --title "IPv4 Or IPv6" --radiolist \
+"Protocol" $r $c 3 \
+"Both" "Both IPv4 and IPv6" ON \
+"IPv4" "Only IPv4" OFF \
+"IPv6" "Only IPv6" OFF 2>results
+
+while read choice
+do
+case $choice in
+	Both) Both;;
+	IPv4) IPv4;;
+	IPv6) IPv6;;
+esac
+done < results
 
 }
 
@@ -54,7 +97,7 @@ DDNS () {
 
 #Install and create CA
 CA () {
-whiptail --backtitle "CA" --title "Installing Certificate Authority" --msgbox "This part will install a certificate authority program and create a certificate for the router interface." 8 78
+whiptail --backtitle "CA" --title "Installing Certificate Authority" --msgbox "This part will install a certificate authority program and create a certificate for the router interface." ${r} ${c}
 wget https://raw.githubusercontent.com/jwepdx/jwepdx.github.io/master/bashscripts/Scripts/CA
 chmod +x CA
 mv CA /usr/bin
@@ -70,8 +113,8 @@ PiVPN () {
 
 #Install Modified Pi-hole
 Pi-hole () {
-    whiptail --backtitle "Pi-hole" --title "Install Pi-hole DNS ad-blocker" --msgbox "You are about to install Pi-hole, a DNS ad-blocker. The following installer has been created by Pi-hole LLC. Pi-Router is not affliiated with Pi-hole LLC" 8 78
-    whiptail --backtitle "Choices" --title "Information" --msgbox "Your answers do not matter except for the IP address, Interface (should be br0), and the blocking over ipv4 and ipv6 (should be both if thy are avalible)" 8 78
+    whiptail --backtitle "Pi-hole" --title "Install Pi-hole DNS ad-blocker" --msgbox "You are about to install Pi-hole, a DNS ad-blocker. The following installer has been created by Pi-hole LLC. Pi-Router is not affliiated with Pi-hole LLC" ${r} ${c}
+    whiptail --backtitle "Choices" --title "Information" --msgbox "Your answers do not matter except for the Pi-hole IP address, Interface (should be br0), and the blocking over ipv4 and ipv6 (should be both if thy are avalible)" ${r} ${c}
 
     #Run Pi-hole Installer
     curl -sSL https://install.pi-hole.net | bash
@@ -88,16 +131,58 @@ Web-Interface () {
 
 }
 
+#Install Additional Configs That Woul Have Been Overwritten
+AdditionalConfigs () {
+
+}
+
 #Restart All Services and Reboot
-Restart () {
+RestartServices () {
 
 }
 
 ###-Installer Starts Here-###
-whiptail --title "Select Packages To Be Installed" --checklist \
-"What Packages Do You Want Install" 20 78 4 \
+whiptail --backtitle "Intorduction" --title "Turn Your Raspberry Pi Into A Router" --yesno "Your Raspberry Pi will be turned into a router. Do you wish to proceed?" ${r} ${c}
+YN=$?
+if [[ $YN == 0 ]]; then
+	GitFiles
+	InstallPackages
+	Back-up
+	Bridge
+	IPTables
+	Hostapd
+else
+	exit 1
+fi
+whiptail --backtitle "Begin" --title "Select Packages To Be Installed" --checklist --separate-output \
+"What Additional Packages Do You Want Install" ${r} ${c} 5 \
 "Unbound" "Recursive DNS Server" ON \
 "DDNS" "Update Records With Your Dynamic DNS Provider" ON \
-"CA" "Create Your" ON \
-"PiVPN" "Allow mounting of local devices" ON \
-"Pi-hole" "Allow mounting of remote devices" ON
+"CA" "Create Your Own Personal Certificate Authority" ON \
+"PiVPN" "Allow Access To Home Network With OpenVPN Protocols" ON \
+"Pi-hole" "Caches The DNS Querys And Blocks Ads" ON 2>results
+while read choice
+do
+case $choice in
+	Unbound) Unbound;;
+	DDNS) DDNS;;
+	CA) CA;;
+	PiVPN) PiVPN;;
+	Pi-Hole) Pi-hole;;
+esac
+done < results
+whiptail --backtitle "Finishing" --title "Almost Done" --yesno "It is know time to install the web interface and restart the component systems. Continue?" ${r} ${c}
+YN=$?
+if [[ $YN == 0 ]]; then
+	Web-Interface
+	AdditionalConfigs
+	RestartServices
+else
+	exit 1
+fi
+whiptail --backtitle "Finished" --title "Rebooting"  --yes-button "Now" --no-button "Later" --yesno "Do you want to reboot now or later, some of the changes made require rebooting." ${r} ${c}
+if [[ $YN == 0 ]]; then
+	reboot
+else
+	exit 0
+fi
